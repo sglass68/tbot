6522b049d566ff7060e3aa05e54465c4172a6032
Simon Glass <sjg@chromium.org>
2020-04-14 20:57:35 -0600

    wip

--- tbot/tc/uboot/__init__.py
+++ tbot/tc/uboot/__init__.py
@@ -3,3 +3,5 @@ from .test import smoke_test
 from .testpy import testpy
 
 __all__ = ("UBootBuilder", "build", "checkout", "smoke_test", "testpy")
+#__all__ = ("UBootBuilder", "BuildmanUBootBuilder", "build", "checkout", "smoke_test", "testpy")
+#from .buildman_builder import BuildmanUBootBuilder
